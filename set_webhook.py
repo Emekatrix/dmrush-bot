70292147adb185595f46d292c3c9a07e051ee93b
Emeka Leverage <sir.emekanzeribe@gmail.com>
2025-06-15 18:07:25 +0100

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -1,7 +1,7 @@
 import telebot
 import os
 
-BOT_TOKEN = os.environ['BOT_TOKEN']
+BOT_TOKEN = os.environ['7566858562:AAF8oJ-UTT-w_8DfeovKwKOGHouVEAMwm9M']
 RAILWAY_URL = os.environ['web-production-10e05.up.railway.app']  # e.g., https://dmrushbot.up.railway.app
 
 bot = telebot.TeleBot(BOT_TOKEN)
