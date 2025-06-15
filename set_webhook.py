c5f92f9dd44d99972e4dffaae7e822a8b19e291e
Emeka Leverage <sir.emekanzeribe@gmail.com>
2025-06-15 17:58:08 +0100

    Create set_webhook.py

---
+++ set_webhook.py
@@ -0,0 +1,13 @@
+import telebot
+import os
+
+BOT_TOKEN = os.environ['BOT_TOKEN']
+RAILWAY_URL = os.environ['RAILWAY_URL']  # e.g., https://dmrushbot.up.railway.app
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+webhook_url = f"{RAILWAY_URL}/webhook"
+success = bot.remove_webhook()
+success = bot.set_webhook(url=webhook_url)
+
+print("Webhook set:", success)
